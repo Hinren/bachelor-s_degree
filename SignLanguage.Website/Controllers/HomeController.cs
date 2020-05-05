649a7bcf310c45e4bdbd4648ecbce34b39a220a7
Patryk Wiśniewski <wisniewskii.patryk@o2.pl>
2020-05-05 22:50:38 +0200

    Delete unnecesessary code, class.

--- SignLanguage.Website/Controllers/HomeController.cs
+++ SignLanguage.Website/Controllers/HomeController.cs
@@ -17,10 +17,5 @@ namespace SignLanguage.Website.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
